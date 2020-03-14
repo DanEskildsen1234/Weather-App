8230be03b26854f9015b04338135772657e4a28a
DanEskildsen <DanEskildsen1234@users.noreply.github.com>
2020-03-14 19:54:05 +0100

    organized files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import WeekView from './components/Weekview';
+import WeekView from './pages/weekview/Weekview';
+import DayView from './pages/dayview/Dayview';
 
 class App extends Component {
   render() {
     return(
       <div className="App">
-        <WeekView />
+        <DayView />
       </div>
     );
   }
