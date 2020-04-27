6044989d2dc6177b74781e30e17c1e87255cf7cc
Dan <dan@mail.com>
2020-04-27 17:12:56 +0200

    Fixed api-key file exclusion

---
+++ src/ApiKey.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "apiKey": "159b4e1b8e9e1237d5e279d2904f03a8"
+}
+
+//kexin11653@mrisemail.com
+//password123
+//159b4e1b8e9e1237d5e279d2904f03a8
+
+//povap96326@newe-mail.com
+//password123
+//d8d5ed7e715c385ac016e449eadb6a9a
